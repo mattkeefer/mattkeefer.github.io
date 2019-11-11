a84f26fead98a6b01c948268b72db2cf185175aa
mattkeefer <mrkeefer8@gmail.com>
2019-11-11 14:54:21 -0500

    projects

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-const logo = document.querySelectorAll('#logo path');
+var slideIndex = 1;
+showSlides(slideIndex);
 
-for(let i=0; i<logo.length; i++) {
-    console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-document.querySelector('video').playbackRate = 1;
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
