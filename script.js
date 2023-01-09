4b970c587d5e9d76a2a24c7e887ecae805eb758c
Matt Keefer <51294591+mattkeefer@users.noreply.github.com>
2023-01-09 18:40:02 -0500

    Removed some backgrounds

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function init() {
     const backgrounds = [
         "url(resources/blue.jpg)",
-        "url(resources/red.jpg)",
-        "url(resources/purple.jpg)",
-        "url(resources/orange-pink.jpg)",
+        // "url(resources/red.jpg)",
+        // "url(resources/purple.jpg)",
+        // "url(resources/orange-pink.jpg)",
         "url(resources/pink-blue.jpg)",
-        "url(resources/blurry.jpg)",
-        "url(resources/blue-pink.jpg)",
+        // "url(resources/blurry.jpg)",
+        // "url(resources/blue-pink.jpg)",
     ];
     var bgImage = backgrounds[Math.floor(Math.random() * backgrounds.length)];
     document.body.style.backgroundImage = bgImage;
